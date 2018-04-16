22512f5917ec32d4ab4c323576f641305bb8a532
Joanna <jmgsese@gmail.com>
2018-04-16 16:04:34 -0500

    Build landing page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="center-text">
+        <p className="company">sehseh</p>
+        <p id="sub-company">web development</p>
+        <p id="text">made with love in new orleans.<br /><a href="mailto: joanna@sehseh.co">say hello.</a></p>
       </div>
     );
   }
